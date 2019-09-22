8808697eb55bfe2b1c1876ade00431398f97b11b
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-22 21:24:32 +0530

    Initial Commit

--- _14_StringBuffer_Methods.java
+++ _14_StringBuffer_Methods.java
@@ -17,27 +17,28 @@ public class _14_StringBuffer_Methods {
         StringBuffer sb = new StringBuffer("Shivam");
 
 
-        // 1) public int length();
+
+        // 1) public synchronized int length();
         System.out.println(sb.length());        // 6
 
 
-        // 2) public int capacity();
+        // 2) public synchronized int capacity();
         System.out.println(sb.capacity());      // 16
 
 
-        // 3) public char charAt(int index);
+        // 3) public synchronized char charAt(int index);
         System.out.println(sb.charAt(3));       // v
         // System.out.println(sb.charAt(30));      // RE: Exception in thread "main" java.lang.StringIndexOutOfBoundsException: index 30,length 6
 
 
 
-        // 4) public void setCharAt(int index, char newChar);
+        // 4) public synchronized void setCharAt(int index, char newChar);
         sb.setCharAt(3,'i');
         System.out.println(sb);                 // Shiiam
         sb.setCharAt(3,'v');
 
 
-        // 5) public StringBuffer append(String s);
+        // 5) public synchronized StringBuffer append(String s);
         // There are multiple append methods(overloaded)
         // We can pass int, float, byte, boolean, double,long.... etc as the parameter to append() .
 
@@ -54,21 +55,21 @@ public class _14_StringBuffer_Methods {
         // Append will add data at the LAST.
 
         // To insert data at specified position, we use insert(), it is also overloaded.
-        // 6) public StringBuffer insert(int index, String s).
+        // 6) public synchronized StringBuffer insert(int index, String s).
         sb.insert(15,"This text is inserted in middle");
         sb.insert(47, true);
 
         System.out.println(sb);     // Shivam Shukla  This text is inserted in middle true    97100true78.0A
 
 
-        // 7) public StringBuffer delete(int begin, int end) -> it deletes character from begin index to end -1 and returns remaining string.
+        // 7) public synchronized StringBuffer delete(int begin, int end) -> it deletes character from begin index to end -1 and returns remaining string.
 
         System.out.println(sb.delete(13,sb.length()));      // Shivam Shukla
 
-        // 8) public StringBuffer deleteCharAt(int index); -> to delete single character;
+        // 8) public synchronized StringBuffer deleteCharAt(int index); -> to delete single character;
         System.out.println(sb.deleteCharAt(6));             // ShivamShukla
 
-        // 9) public SB reverse();  -> In string, we do not have reverse() , but in SB we have reverse() to reverse order of characters!
+        // 9) public synchronized SB reverse();  -> In string, we do not have reverse() , but in SB we have reverse() to reverse order of characters!
         System.out.println(sb.reverse());                   // alkuhSmavihS
         System.out.println(sb);                             // alkuhSmavihS
         sb.reverse();
@@ -78,7 +79,7 @@ public class _14_StringBuffer_Methods {
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // 10) public void setLength(int length);
+        // 10) public synchronized void setLength(int length);
         // Suppose we want only first 6 characters, so we can set the length of our existing SB object to 6.
         sb.setLength(6);
         System.out.println(sb);                             // Shivam
@@ -91,7 +92,7 @@ public class _14_StringBuffer_Methods {
         System.out.println(sb);                             // Shivam    Shukla
 
 
-        // 11) public void ensureCapacity(int capacity);
+        // 11) public synchronized void ensureCapacity(int capacity);
         // If we want to dynamically increase the capacity of SB, then we can use ensureCapacity();
 
         sb.ensureCapacity(100);
@@ -106,19 +107,11 @@ public class _14_StringBuffer_Methods {
         System.out.println(sb.capacity());              // 1000
 
 
-        // 12) public void trimToSize();
+        // 12) public synchronized void trimToSize();
         // To deallocate the memory which is useless.The capacity will be equal to the length of the String Contained!
 
         sb.trimToSize();
         System.out.println(sb.capacity());              // 16                   (Shivam    Shukla)
-
-
-
-
-
-
-
-
-
+        
     }
 }
